2a44a248b802480ff2662c1af56d6bc3d9459a4f
Aditya <adityamaurya0361@gmail.com>
2026-08-21 09:33:04 +0530

    Time: 0 ms (100%), Space: 10.7 MB (72.1%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string result;
+        int count =0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='-') continue;
+            if(count == k){
+                result.push_back('-');
+                count=0;
+            }
+            result.push_back(toupper(s[i]));
+            count++;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
